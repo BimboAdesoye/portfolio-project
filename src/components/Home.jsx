10053de72e7bb2c4480dd6d59324de479ea69325
Abimbola Adesoye <bimboadesoye44@gmail.com>
2023-08-23 11:04:08 +0100

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,65 @@
+import "../styles/Home.css";
+import Bootstrap from "../assets/Bootstrap.svg";
+import NodeJs from "../assets/NodeJs.svg";
+import HTML from "../assets/HTML.svg";
+import JavaScript from "../assets/logos_javascript.svg";
+import React from "../assets/React.svg";
+import CSS from "../assets/CSS.svg";
+import ProfilePhoto from "../assets/ProfilePhoto.svg";
+import MailIcon from "../assets/MailIcon.svg";
+import DownloadIcon from "../assets/DownloadIcon.svg";
+import GithubIcon from "../assets/GithubIcon.svg";
+import Saly from "../assets/Saly-13.svg";
+
+const Home = () => {
+  return (
+    <div className="mycontainer">
+      <div className="home">
+        <div className="profile">
+          <p className="profile-title">Full Stack Developer</p>
+          <h1 className="profile-name">Abimbola Adesoye</h1>
+          <p className="profile-desc">
+            I am Abimbola, a passionate frontend web developer dedicated to
+            crafting seamless and engaging online experiences. With a keen eye
+            for design and a proficiency in HTML, CSS, and JavaScript, I
+            transform ideas into visually appealing and user-friendly websites
+            that leave a lasting impression.
+          </p>
+          <div className="tech-stack">
+            <img src={Bootstrap} alt="" />
+            <img src={NodeJs} alt="" />
+            <img src={HTML} alt="" />
+            <img src={JavaScript} alt="" />
+            <img src={React} alt="" />
+            <img src={CSS} alt="" />
+          </div>
+          <div className="contact-me">
+            <img src={ProfilePhoto} alt="" />
+            <div className="info">
+              <a>
+                <img src={MailIcon} alt="" />
+                <p className="email">Email Me</p>
+                <img src={DownloadIcon} alt="" />
+              </a>
+              <a
+                href="https://github.com/BimboAdesoye"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={GithubIcon} alt="" />
+                <p className="github">Github</p>
+                <img src={DownloadIcon} alt="" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="Picture">
+          {/* <div className="ellipse"></div> */}
+          <img className="saly" src={Saly} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import "../styles/Navbar.css";
+import { Link } from "react-router-dom";
+import MoonIcon from "../assets/icon-moon.svg";
+import SunIcon from "../assets/icon-sun.svg";
+import { useState } from "react";
+
+const Navbar = ({ styleBody }) => {
+  const [lightMode, setLightMode] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const navClass = "nav-list";
+
+  function toggleNavbar() {
+    setToggle(!toggle);
+  }
+
+  function handleLightMode() {
+    setLightMode(!lightMode);
+    if (toggle) {
+      styleBody();
+    }
+  }
+  return (
+    <nav className="Navbar mycontainer">
+      <div className="logo">
+        <p>
+          <Link to={"/"} className="logo">
+            Abimbola.
+          </Link>
+        </p>
+      </div>
+      <div className="other">
+        <ul className={`${navClass} ${toggle ? "toggled" : null}`}>
+          <li>
+            <Link className="nav-link" to={"/Projects"}>
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link">Download Resume</Link>
+          </li>
+          <li>
+            <a
+              href="https://abimbola.hashnode.dev/"
+              target="_blank"
+              rel="noreferrer"
+              className="nav-link"
+            >
+              Blog
+            </a>
+          </li>
+          <li>
+            <Link className="nav-link" to={"/Contact"}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div>
+          <img
+            onClick={handleLightMode}
+            src={lightMode ? MoonIcon : SunIcon}
+            alt=""
+          />
+        </div>
+        <div className="hamburger" onClick={toggleNavbar}>
+          <div className="burger one"></div>
+          <div className="burger two"></div>
+          <div className="burger three"></div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
